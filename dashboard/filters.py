7379154753f5482976c96743c7ba75e0a71f5282
shafiqlipse <ssekittoshafiq@gmail.com>
2024-02-17 15:17:56 +0300

    pay

--- dashboard/filters.py
+++ dashboard/filters.py
@@ -1,11 +1,12 @@
 import django_filters
 from .models import *
+from django_select2.forms import Select2Widget
 
 
 class OfficialFilter(django_filters.FilterSet):
     class Meta:
         model = school_official
-        fields = ("name", "gender", "role","email")
+        fields = ("name", "gender", "role", "email")
 
 
 class schoolsFilter(django_filters.FilterSet):
@@ -14,12 +15,22 @@ class schoolsFilter(django_filters.FilterSet):
         fields = ("school_name", "EMIS", "region", "district")
 
 
+
 class AthleteFilter(django_filters.FilterSet):
     class Meta:
         model = Athlete
         fields = ("fname", "sport", "classroom", "gender")
+        widgets = {
+            "sport": Select2Widget,
+        }
+
 
 class AthletesFilter(django_filters.FilterSet):
     class Meta:
         model = Athlete
         fields = ("fname", "sport", "school", "gender")
+        widgets = {
+            "sport": Select2Widget,
+            "school": Select2Widget,
+            # "sport": Select2Widget,
+        }
