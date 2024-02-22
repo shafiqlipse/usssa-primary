87fb08e6d4d27780501a1210762130a8e12acde9
shafiqlipse <ssekittoshafiq@gmail.com>
2024-02-22 19:40:41 +0300

    pagination

--- primary/urls.py
+++ primary/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from dashboard.views import *
 from accounts.views import *
@@ -17,4 +19,4 @@ urlpatterns = [
     # path("album/", album, name="album"),
     path("select2/", include("django_select2.urls")),
     path("calculate_age_choices/", calculate_age_choices, name="calculate_age_choices"),
-]
+]  # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
