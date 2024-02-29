18b8c1aac616dd2325c0591b16e7b260b0ebda9c
shafiqlipse <ssekittoshafiq@gmail.com>
2024-02-29 08:53:34 +0300

    js

--- dashboard/static/scripts/app.js
+++ dashboard/static/scripts/app.js
@@ -80,11 +80,26 @@ const ToggleAccount = () => {
 // }
 
 // });
-
 function displayImage(input) {
   var reader = new FileReader();
   reader.onload = function (e) {
     document.getElementById("image-preview").src = e.target.result;
+    document.getElementById("image-previews").src = e.target.result; // Corrected ID
+    document.getElementById("image-previewg").src = e.target.result;
+  };
+  reader.readAsDataURL(input.files[0]);
+}
+function displayImage1(input) {
+  var reader = new FileReader();
+  reader.onload = function (e) {
+    document.getElementById("image-previews").src = e.target.result;
+  };
+  reader.readAsDataURL(input.files[0]);
+}
+function displayImage2(input) {
+  var reader = new FileReader();
+  reader.onload = function (e) {
+    document.getElementById("image-previewg").src = e.target.result; // Corrected ID
   };
   reader.readAsDataURL(input.files[0]);
 }
