426e32bdab29901cecd0903ef72774d646f253d7
shafiqlipse <ssekittoshafiq@gmail.com>
2024-04-03 16:08:54 +0300

    diactive

---
+++ registration/forms.py
@@ -0,0 +1,10 @@
+# forms.py
+
+from django import forms
+from .models import Athlete
+
+
+class AthleteSelectionForm(forms.Form):
+    athletes = forms.ModelMultipleChoiceField(
+        queryset=Athlete.objects.all(), widget=forms.CheckboxSelectMultiple
+    )
