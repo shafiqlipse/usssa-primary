3fd0cbd3469089a2012f6fa13735b98364d92fa0
shafiq <ssekittoshafiq@gmail.com>
2025-03-18 12:49:45 +0300

    log

--- accounts/decorators.py
+++ accounts/decorators.py
@@ -3,7 +3,9 @@ from django.http import HttpResponseForbidden
 from functools import wraps
 
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from functools import wraps
+from django.shortcuts import redirect, render
+from django.contrib import messages
 
 
 def school_required(view_func):
@@ -37,17 +39,29 @@ def staff_required(view_func):
 
     return _wrapped_view
 
-
 def anonymous_required(view_func):
     """
     Decorator to ensure that the view is only accessible to anonymous users (not logged in).
+    If the user is authenticated, redirect them to the appropriate dashboard based on their role.
     """
 
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
         if request.user.is_authenticated:
-            return render(request, "dashboard/dashboard.html")  # Customize this template
+            user = request.user
+
+            # Check user roles and redirect accordingly
+            if getattr(user, 'is_school', False):
+                messages.success(request, "School login successful.")
+                return redirect("school_dashboard")
+            elif getattr(user, 'is_admin', False):
+                messages.success(request, "Officer login successful.")
+                return redirect("officer_dashboard")
+            else:
+                messages.success(request, "Login successful.")
+                return redirect("dashboard")
         else:
+            # If the user is not authenticated, allow access to the view
             return view_func(request, *args, **kwargs)
 
     return _wrapped_view
